f853c35f71781cfd48a472405b10915fbea75009
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-08-05 15:49:25 -0300

    variables added to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Be Vietnam Pro, sans-serif',
+    },
+    extend: {
+      backgroundImage: {
+        'bg-image': 'url("/bg.jpg")',
+      },
+      colors: {
+        'light-violet': '#393f6e',
+        violet: '#343964',
+        gray: '#e2e4f3',
+        'dark-gray': '#8b8eab',
+        'light-blue': '#3e9fff',
+        red: '#dd524c',
+        skin: '#ffecc8',
+      },
+    },
   },
   plugins: [],
 };
