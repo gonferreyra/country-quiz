09766cfe5e0b7a8d80fcacf2ab06c86199ef195c
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-08-06 11:51:40 -0300

    edit styles conditions with clsx

--- src/components/NumberBtn.tsx
+++ src/components/NumberBtn.tsx
@@ -1,10 +1,12 @@
 import { clsx } from 'clsx';
+import { Answer } from '../types/types';
 
 type NumberBtnProps = {
   children: React.ReactNode;
   index: number;
   onClick: (index: number) => void;
   questionIndex: number;
+  answers: Answer[];
 };
 
 export default function NumberBtn({
@@ -12,11 +14,13 @@ export default function NumberBtn({
   index,
   onClick,
   questionIndex,
+  answers,
 }: NumberBtnProps) {
   return (
     <button
-      className={clsx('text-l h-10 w-10 rounded-full bg-violet transition', {
-        'from-gradient1 to-gradient2 bg-gradient-to-b': index === questionIndex,
+      className={clsx('h-10 w-10 rounded-full bg-violet text-sm transition', {
+        'from-gradient1 to-gradient2 bg-gradient-to-r':
+          index === questionIndex || answers[index],
       })}
       onClick={() => onClick(index)}
     >
